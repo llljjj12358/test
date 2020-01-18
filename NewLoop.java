df2f239634fde5c303dced767a28b67242412c49
DA105_42 <wiwi3583141@gmail.com>
2020-01-18 15:01:08 +0800

    float add

--- NewLoop.java
+++ NewLoop.java
@@ -5,6 +5,7 @@ import java.io.Console;
 public class LoopPratice {
 	public static void main(String[] args) {
 		int a=0,b=1;
+		float test=1.0;
 		for (int j = 1; j <= 6; j++) {
 			for (int i = 2; i <= 6; i++) {
 				
