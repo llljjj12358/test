0987a10496824812252917ebafc12ae52b9c9bbb
DA105_42 <wiwi3583141@gmail.com>
2020-01-18 14:44:37 +0800

    hotfix

--- NewLoop.java
+++ NewLoop.java
@@ -4,7 +4,7 @@ import java.io.Console;
 
 public class LoopPratice {
 	public static void main(String[] args) {
-		int a=0;
+		int a=0,b=1;
 		for (int j = 1; j <= 6; j++) {
 			for (int i = 2; i <= 6; i++) {
 				
